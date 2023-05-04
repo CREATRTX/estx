9b9499fccf50806f3711a8acbe477c9f85e37256
creeeeeee <48557016+creeeeeee@users.noreply.github.com>
2023-05-04 20:13:45 +0800

    1

---
+++ 2.py
@@ -0,0 +1,24 @@
+image_size = 1024
+vit_patch_size = 16
+image_embedding_size = image_size // vit_patch_size
+sam = Sam(
+    image_encoder=ImageEncoderViT(
+        depth=encoder_depth,
+        embed_dim=encoder_embed_dim,
+        img_size=image_size,
+        mlp_ratio=4,
+        norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
+        num_heads=encoder_num_heads,
+        patch_size=vit_patch_size,
+        qkv_bias=True,
+        use_rel_pos=True,
+        global_attn_indexes=encoder_global_attn_indexes,
+        window_size=14,
+        out_chans=prompt_embed_dim,
+    ),
+    prompt_encoder=PromptEncoder(
+        embed_dim=prompt_embed_dim,
+        image_embedding_size=(image_embedding_size, image_embedding_size),
+        input_image_size=(image_size, image_size),
+        mask_in_chans=16,
+    ),
